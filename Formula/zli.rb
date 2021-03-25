d7282fa7ac06e77d77e91acb936c41ce00d9d349
Nabeelperson <nabeel.younis@yahoo.com>
2021-03-25 11:26:17 -0400

    4.10.0  - Added check for linux

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -1,10 +1,11 @@
 require "language/node"
+require "os"
 
 class Zli < Formula
   desc "BastionZero cli"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/4.9.0/zli-4.9.0.tar.gz"
-  sha256 "553b7a82a1b4b5015acc9bbe645afcca1fb351e92dbfe6514d7355e8df145136"
+  url "https://github.com/bastionzero/zli/releases/download/4.10.0/zli-4.10.0.tar.gz"
+  sha256 "6b044d3b475070be57e664859edd46ffd7d8dee7e88fb0a5ed55f6f108b1d63b"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git"
 
@@ -14,11 +15,17 @@ class Zli < Formula
     system "npm", "install", *Language::Node.local_npm_install_args
     system "npm", "run", "release"
     rm "./bin/zli-win.exe"
-    rm "./bin/zli-linux"
-    bin.install "bin/zli-macos" => "zli"
+
+    if OS.linux?
+      rm "./bin/zli-macos"
+      bin.install "bin/zli-linux" => "zli"
+    else
+      rm "./bin/zli-linux"
+      bin.install "bin/zli-macos" => "zli"
+    end
   end
 
   test do
-    system "zli", "help"
+    system "zli", "config"
   end
 end
