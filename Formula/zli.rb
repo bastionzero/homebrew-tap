64212a10b94455f3226afc95b18d04cdb7cffbac
Sid Premkumar <sidpremkumar@Sids-MacBook-Pro.local>
2021-11-19 10:54:44 -0500

    Specify master branch

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -7,7 +7,7 @@ class Zli < Formula
   url "https://github.com/bastionzero/zli/releases/download/5.1.59/zli-5.1.59.tar.gz"
   sha256 "ad3aa9fb6cd0d48283b952760b17a3b1af41609fe99e620850ced120b1dd4a27"
   license "Apache-2.0"
-  head "https://github.com/bastionzero/zli.git"
+  head "https://github.com/bastionzero/zli.git", branch: "master"
 
   depends_on "go@1.17" => :build
   depends_on "node@14"
