41fdac4d25b509fd3840c4b266b2b03658076daa
Sid Premkumar <sidpremkumar@Sids-MacBook-Pro.local>
2021-11-19 09:11:36 -0500

    Add zli stage formula

---
+++ Formula/zli-stage.rb
@@ -0,0 +1,32 @@
+require "language/node"
+require "os"
+
+class ZliStage < Formula
+  desc "BastionZero cli - Stage"
+  homepage "https://www.bastionzero.com"
+  url "https://github.com/bastionzero/zli/releases/download/5.1.59/zli-5.1.59.tar.gz"
+  sha256 "ad3aa9fb6cd0d48283b952760b17a3b1af41609fe99e620850ced120b1dd4a27"
+  license "Apache-2.0"
+  head "https://github.com/bastionzero/zli.git"
+
+  depends_on "go@1.17" => :build
+  depends_on "node@14"
+
+  def install
+    system "npm", "install", *Language::Node.local_npm_install_args
+    system "npm", "run", "release-prod"
+    rm "./bin/zli-win.exe"
+
+    if OS.linux?
+      rm "./bin/zli-macos"
+      bin.install "bin/zli-linux" => "zli-stage"
+    else
+      rm "./bin/zli-linux"
+      bin.install "bin/zli-macos" => "zli-stage"
+    end
+  end
+
+  test do
+    system "zli-stage", "configure"
+  end
+end
