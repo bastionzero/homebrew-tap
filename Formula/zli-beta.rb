b75da8318a6e82b713cbeaffb6ff4e177bd1c42b
Sid Premkumar <sid.premkumar@gmail.com>
2021-11-19 14:57:33 -0500

    Add zli beta formula (#38)
    * Add zli beta formula
    
    Co-authored-by: Sid Premkumar <sidpremkumar@Sids-MacBook-Pro.local>

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -1,11 +1,11 @@
 require "language/node"
 require "os"
 
-class Zli < Formula
-  desc "BastionZero cli"
+class ZliBeta < Formula
+  desc "BastionZero cli - Beta"
   homepage "https://www.bastionzero.com"
-  url "$URL"
-  sha256 "$SHA256"
+  url "https://github.com/bastionzero/zli/releases/download/5.1.59/zli-5.1.59.tar.gz"
+  sha256 "ad3aa9fb6cd0d48283b952760b17a3b1af41609fe99e620850ced120b1dd4a27"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
@@ -19,14 +19,14 @@ class Zli < Formula
 
     if OS.linux?
       rm "./bin/zli-macos"
-      bin.install "bin/zli-linux" => "zli"
+      bin.install "bin/zli-linux" => "zli-beta"
     else
       rm "./bin/zli-linux"
-      bin.install "bin/zli-macos" => "zli"
+      bin.install "bin/zli-macos" => "zli-beta"
     end
   end
 
   test do
-    system "zli", "configure"
+    system "zli-beta", "configure"
   end
 end
