09c4abf3b0c98fe5da5346bf27621f5b58e6d4dd
tflpd <aflpd@bu.edu>
2022-01-13 00:18:07 +0200

    Zli version: 5.3.2

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -4,15 +4,10 @@ require "os"
 class Zli < Formula
   desc "BastionZero cli"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/5.2.6/zli-5.2.6.tar.gz"
-  sha256 "f6aafe2d7a7758a665234e62c17789a414f75299a816a0ea6254b02683746290"
+  url "https://github.com/bastionzero/zli/releases/download/5.3.2/zli-5.3.2.tar.gz"
+  sha256 "016f880692b36e028f3b55842f600d8199d6710a339335800593c58723917d7a"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-5.2.6"
-    sha256 cellar: :any_skip_relocation, big_sur: "b6be2c0119a9ee589396d427811c291f53f9785a8c1ee6045473439a3d0d4da0"
-  end
 
   depends_on "go@1.17" => :build
   depends_on "node@14"
