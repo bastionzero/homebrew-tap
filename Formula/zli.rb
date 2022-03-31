cc9f5d1efa5d4e8d4b3dd099c87452a9d91c98c4
Sid Premkumar <sid.premkumar@gmail.com>
2022-03-31 15:38:08 -0400

    Zli version: 5.6.14

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -4,8 +4,8 @@ require "os"
 class Zli < Formula
   desc "BastionZero cli"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/5.6.13/zli-5.6.13.tar.gz"
-  sha256 "bac15ad9b8f5e8df6ed8e7ec56237d7696c9f90dd7b71ef5f25598ba897b5fa2"
+  url "https://github.com/bastionzero/zli/releases/download/5.6.14/zli-5.6.14.tar.gz"
+  sha256 "b92a6ea999d28b66ee21d4e9a4182bf224e8c4121ac31865b6afce9dfa7a22c2"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
