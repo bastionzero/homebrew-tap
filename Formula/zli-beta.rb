d7e6419f29b3a783e3593e5e40dbaadfdf0198ba
Sebby Lipman <lipmas93@gmail.com>
2022-05-11 13:44:38 -0400

    Zli version: 6.3.11-beta (#75)

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -4,8 +4,8 @@ require "os"
 class ZliBeta < Formula
   desc "BastionZero cli - Beta"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/6.1.1-beta/zli-6.1.1-beta.tar.gz"
-  sha256 "390120ea267c9c931087fb2533a2297a7a8f3542f5473c2770f4b2d704c47ce1"
+  url "https://github.com/bastionzero/zli/releases/download/6.3.11-beta/zli-6.3.11-beta.tar.gz"
+  sha256 "6f0fed4985ab9dc1b503b1165b0238ad95d0ddb20b9af686cade4dbbbe2973b7"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
