936d6ca7b878749808b2b80b39c7b881b6a2d1b8
Sebby Lipman <sebby@commonwealthcrypto.com>
2022-10-13 07:27:42 -0600

    Zli version: 6.11.8 (#112)

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -4,8 +4,8 @@ require "os"
 class Zli < Formula
   desc "BastionZero cli"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/6.11.1/zli-6.11.1.tar.gz"
-  sha256 "7f2447cdaff6090bb934748b4febec030be2e12cbb4a9f7856a36311894b057a"
+  url "https://github.com/bastionzero/zli/releases/download/6.11.8/zli-6.11.8.tar.gz"
+  sha256 "aadf87d986d207e0d1492afb90150d3652d7212799e2eb2204967e443e2c28e2"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
