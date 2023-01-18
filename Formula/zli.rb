0fb4d1c9a30bc24848235c0b3efad57b58524e72
John C. Merfeld <John.Merfeld@gmail.com>
2023-01-18 17:36:51 -0500

    Zli version: 6.15.12 (#134)

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -4,8 +4,8 @@ require "os"
 class Zli < Formula
   desc "BastionZero cli"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/6.14.3/zli-6.14.3.tar.gz"
-  sha256 "df6cdb1080a179f7860f46ca7aa5e99545e5cfdb2577c13b8b360513358edc84"
+  url "https://github.com/bastionzero/zli/releases/download/6.15.12/zli-6.15.12.tar.gz"
+  sha256 "e394178f6de45b01f787af69b45e80a7c5606744661a7a9d019ca31206d4afb8"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
