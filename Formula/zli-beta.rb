b1dfc0530b30b177726a6e10480f081b0131edb5
John C. Merfeld <John.Merfeld@gmail.com>
2023-06-05 14:50:41 -0700

    Zli version: 6.21.2-beta (#169)

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -4,8 +4,8 @@ require "os"
 class ZliBeta < Formula
   desc "BastionZero cli - Beta"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/6.21.1-beta/zli-6.21.1-beta.tar.gz"
-  sha256 "85dafe43bf9729cde749e16aba5b24257cd0de21b4deb1ab24a5da23ca71f311"
+  url "https://github.com/bastionzero/zli/releases/download/6.21.2-beta/zli-6.21.2-beta.tar.gz"
+  sha256 "58237e0b5c94aed6d2f299fcafced2378dd85b9f3332abccccca13bc304bfc67"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
