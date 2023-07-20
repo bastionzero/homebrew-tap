d7f0f25e72b2eacc0be70b1e40f23ab4a41601a2
lgmugnier <10800317+lgmugnier@users.noreply.github.com>
2023-07-20 22:20:18 +0000

    zli-beta: add 6.25.6-beta bottle.

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -9,6 +9,13 @@ class ZliBeta < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-beta-6.25.6-beta"
+    sha256 cellar: :any_skip_relocation, ventura:  "40c80ff25d04de3aa3b38a104a53d10a6e65bc7326ea5ffca27964f0bad4d0fd"
+    sha256 cellar: :any_skip_relocation, monterey: "fd9a262cbbd31806c2405231a3190b7658cc9c83449b3e5b06b0745d19331f6a"
+    sha256 cellar: :any_skip_relocation, big_sur:  "c030a8cd3813fd80e720a3f4c22d163cf331668448b59d84e11ecd8688ea6a49"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@14"
 
