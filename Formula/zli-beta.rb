435285d3c558c8e434944dc8cfe80b256ff0df0c
tflpd <aflpd@bu.edu>
2023-07-21 12:05:20 +0300

    Zli version: 6.25.7-beta

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -4,17 +4,10 @@ require "os"
 class ZliBeta < Formula
   desc "BastionZero cli - Beta"
   homepage "https://www.bastionzero.com"
-  url "https://github.com/bastionzero/zli/releases/download/6.25.6-beta/zli-6.25.6-beta.tar.gz"
-  sha256 "6294ea7fc8c74609be15b366fd3c3e45c61295ccfbecf3654643d9910f9a74db"
+  url "https://github.com/bastionzero/zli/releases/download/6.25.7-beta/zli-6.25.7-beta.tar.gz"
+  sha256 "5842ced7b8ed7fa1e3037f14516663205c0beafdc0bfc651775e74f2b1fb4203"
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-beta-6.25.6-beta"
-    sha256 cellar: :any_skip_relocation, ventura:  "40c80ff25d04de3aa3b38a104a53d10a6e65bc7326ea5ffca27964f0bad4d0fd"
-    sha256 cellar: :any_skip_relocation, monterey: "fd9a262cbbd31806c2405231a3190b7658cc9c83449b3e5b06b0745d19331f6a"
-    sha256 cellar: :any_skip_relocation, big_sur:  "c030a8cd3813fd80e720a3f4c22d163cf331668448b59d84e11ecd8688ea6a49"
-  end
 
   depends_on "go@1.20" => :build
   depends_on "node@14"
