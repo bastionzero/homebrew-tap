3859a8a9d3250eb902d3550829539ed2730e28f5
lgmugnier <10800317+lgmugnier@users.noreply.github.com>
2023-07-25 18:59:29 +0000

    zli-beta: add 6.25.8-beta bottle.

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -9,6 +9,16 @@ class ZliBeta < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-beta-6.25.8-beta"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cb311ab8ee271770fe4b3aad0dda7f0952407f9fc31f61bd4a9ce2c1d508d295"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "82cfe6ce7355c3d5bac8c2da10be423114b164272b5bf441868e78f0a62f2aba"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1f7f88aed5c2d436e561f98c6b2fe1db18c692319dbd871b2abec3e345daf94"
+    sha256 cellar: :any_skip_relocation, ventura:        "33f52623b083036790ff2bf930844a3278f364041166a9fd7f2c365364bd53c2"
+    sha256 cellar: :any_skip_relocation, monterey:       "5a38f5d172469fe8d0e1232ab7d6430984de34ccca8330f438b9bcd694c7daa9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1aedea3c835d87aec731258dd3124abe4bce29b54f1c584859dd4c301e7eadea"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@14"
 
