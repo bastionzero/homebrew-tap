630ed58440fed5758001a92f4a64a9b3991de319
Sebby <lipmas93@gmail.com>
2023-07-27 18:10:41 -0600

    style cop

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -4,10 +4,11 @@ require "os"
 class ZliBeta < Formula
   desc "BastionZero cli - Beta"
   homepage "https://www.bastionzero.com"
+  # https://github.com/Homebrew/brew/issues/15169#issuecomment-1504229816
   url "https://api.github.com/repos/bastionzero/zli/releases/assets/118622363",
     headers: [
       "Accept: application/octet-stream",
-      "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}"
+      "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}",
     ]
   sha256 "5fe26009ecfdf2c2f2f4f7b9b99b6430247c2b1ee5fdca1a5e883e27a035fa8b"
   license "Apache-2.0"
