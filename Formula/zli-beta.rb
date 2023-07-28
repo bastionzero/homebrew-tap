b1f781a5952174e9fe518939d082d92898d87fb8
Sebby <lipmas93@gmail.com>
2023-07-28 13:24:22 -0600

    Try using custom curl downloader for private github repos
    Code taken from https://gist.github.com/minamijoyo/3d8aa79085369efb79964ba45e24bb0e

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -1,14 +1,11 @@
 require "language/node"
 require "os"
+require_relative "lib/private_strategy"
 
 class ZliBeta < Formula
   desc "BastionZero cli - Beta"
   homepage "https://www.bastionzero.com"
-  url "https://api.github.com/repos/bastionzero/zli/releases/assets/118622363",
-    :headers => [
-      "Accept: application/octet-stream",
-      "Authorization: bearer #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}",
-    ]
+  url "https://github.com/bastionzero/zli/releases/download/6.27.0-beta/zli-6.27.0-beta.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
   version "6.27.0-beta"
   sha256 "5fe26009ecfdf2c2f2f4f7b9b99b6430247c2b1ee5fdca1a5e883e27a035fa8b"
   license "Apache-2.0"
