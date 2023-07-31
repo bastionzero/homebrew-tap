d3b333edb70e223b35462f34ab385b1cf5f6e3b2
Sebby <lipmas93@gmail.com>
2023-07-30 20:40:40 -0600

    test

--- lib/git_hub_private_repository_download_strategy.rb
+++ lib/git_hub_private_repository_download_strategy.rb
@@ -45,7 +45,6 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   require "utils/github"
 
   def initialize(url, name, version, **meta)
-    raise CurlDownloadStrategyError, "url is #{url}."
     super
     parse_url_pattern
     set_github_token
