133a943f969846dd16730aa2f64a88b786c03cf1
Sebby Lipman <sebby@commonwealthcrypto.com>
2023-12-08 21:14:23 +0000

    zli: add 6.35.3 bottle.

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -11,6 +11,14 @@ class Zli < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-6.35.3"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f62d9754767524eeb061d3986aca7238c2f39d79443c342c8f52d04b3221b19e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "0cd1b81d8c16346b3db5a23ee2f5cdde2951309125d78f45621111588eab87bf"
+    sha256 cellar: :any_skip_relocation, ventura:        "d6c2f87ee706cc9dee62ca73a432eb937f42ca1562d3cd5471f5475726067586"
+    sha256 cellar: :any_skip_relocation, monterey:       "efac04d1c80bad246dc8c7325cf56de701706c807e67f3f582621c6d4d65904a"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@14"
 
