d9db1bfd6c96399ee2a28f33b96e7f10e3a937dd
John C. Merfeld <John.Merfeld@gmail.com>
2024-01-10 02:02:51 +0000

    zli-beta: add 6.36.0-beta bottle.

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -11,6 +11,12 @@ class ZliBeta < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-beta-6.36.0-beta"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "2ec70d55821650c480b419dbf90e58357aff837ea788b77c9f5eaf9d191664bb"
+    sha256 cellar: :any_skip_relocation, monterey:       "2fcbb23e3d07887f8bf902a5fec214a7e5945bf1cfbeb2bd8951ab74c9619a17"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@14"
 
