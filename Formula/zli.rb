74eb529cd01d0eac0ed8a0f2c3c7f4991eeefddd
Saicharan Dadireddy <29279202+SaiDadireddy@users.noreply.github.com>
2024-01-18 21:45:18 +0000

    zli: add 6.36.2 bottle.

--- Formula/zli.rb
+++ Formula/zli.rb
@@ -11,6 +11,14 @@ class Zli < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-6.36.2"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ae3533be7ba1cc411cd9b8f17a697052b08c53e3e1a3b254d4fe4e75f69a0988"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "044ae054e2b0a0238a5dac350c6141c89caedf31c7c54883146aaebbaf708cf8"
+    sha256 cellar: :any_skip_relocation, ventura:        "8a4ff55706ec2ba97f920ca9a20ef10b894b2f1a530af921c80cd2a59480c913"
+    sha256 cellar: :any_skip_relocation, monterey:       "9a1ad119b2262a014deb367301a688a54971ef7119e4647d53b656c8b2cbde35"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@14"
 
