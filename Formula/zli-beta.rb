6271aa9196ff7a3378774a882c0719051a14c9a0
Sebby Lipman <sebby@commonwealthcrypto.com>
2024-05-28 22:18:49 +0000

    zli-beta: add 6.36.16-beta bottle.

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -11,6 +11,15 @@ class ZliBeta < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-beta-6.36.16-beta"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "0bd87e63a8888c2fa28a4e2c90a2b0727e0a39ae9805ce00f2bbf49b4657a543"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5b75186efb107fd03ec60881ae9125ee7bad95f5a56eb1844a5e6a107f617677"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e236e8b772e66e7683d416f7ab791a4df83a59ae7f5fffb503ac69aac261fc33"
+    sha256 cellar: :any_skip_relocation, ventura:        "08dc2a18a55e192b035ef10c382f5535597b1f53f2e430b980735d930cabcd00"
+    sha256 cellar: :any_skip_relocation, monterey:       "69127ba73f35783766bb0a3d9f08e4d19f5a146064a06587545e5ec035d6d368"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@20" => :build
 
