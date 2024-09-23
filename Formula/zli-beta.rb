b09eae14010defaf1c53ab52cf30acbb5f266d3a
Sebby Lipman <sebby@cloudflare.com>
2024-09-23 19:40:50 +0000

    zli-beta: add 6.36.19-beta bottle.

--- Formula/zli-beta.rb
+++ Formula/zli-beta.rb
@@ -11,6 +11,15 @@ class ZliBeta < Formula
   license "Apache-2.0"
   head "https://github.com/bastionzero/zli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/bastionzero/homebrew-tap/releases/download/zli-beta-6.36.19-beta"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "677d2d67dddd46576a7eaded3a88a43869dae22b4e64076cbaf77ab5ea0297a1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f713f792ccbdb151f1630f2806e33e705b4cacf2171bea72c17beb189779facf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f973d6f324097c888484640c036eae4e203769e23d6670f2512ebc6e63d9527e"
+    sha256 cellar: :any_skip_relocation, ventura:        "ea8499c8c430701119257968543a2d91d0dd8552f0fd0d96e302a43e9bd0227e"
+    sha256 cellar: :any_skip_relocation, monterey:       "f9d8e920993177b9c16cba11e55b9f4a5cccaa4be43c55de6135929202b59be6"
+  end
+
   depends_on "go@1.20" => :build
   depends_on "node@20" => :build
 
